f790dca36dfc0e8051da271b5b3c53360f9379a9
kanjitta <jittakan2539@gmail.com>
2024-05-24 11:16:16 +0700

    Add first feature: Add input to product list and display.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Feature 1: Add unput to product
+
 //declare a starting value to store data.
 let upload = [];
 let idCounter = 0;
@@ -9,7 +11,27 @@ function isImgUrl(imageUrl) {
 	return /\.(jpg|jpeg|png|gif)$/.test(input.pathname);
 }
 
-//Event when clicking 'Create Product' button in Eventlistener
+//Make function to upload
+function displayUpload(upload) {
+	const displaySection = document.getElementById("displaySection");
+	const card = document.createElement("div");
+	card.className = "inline-flex bg-white w-full mx-auto justify-left";
+
+	card.innerHTML = `
+		<label class="items-center mt-2 text-2xl px-4">
+			<input type="checkbox" class="w-3 h-3 text-2xl accent-rose-600" data-id="${upload.id}" onchange="toggleLike(event)">
+			
+		</label>
+
+        <img src="${upload.imageUrl}" alt="${upload.productName}" class="w-[200px] aspect-[4/3] mb-4 object-cover">
+        <p class="text-gray-700 font-semibold text-2xl text-balance overflow-hidden my-4 p-4">${upload.productName}</p><br>
+		<p class="text-gray-700 font-semibold text-2xl text-balance overflow-hidden my-4 p-4">$${upload.price}</p>
+    `;
+
+	displaySection.appendChild(card);
+}
+
+//Event when clicking 'Create Product' button
 document
 	.getElementById("bt-product")
 	.addEventListener("click", function (event) {
@@ -20,77 +42,10 @@ document
 		const productName = document.getElementById("productName").value;
 		const price = document.getElementById("price").value;
 		const image = document.getElementById("image").value;
-
-		//declare a starting value to store data.
-		let upload = [];
-		let idCounter = 0;
-		let totalBuys = 0;
-
-		//function to check the file type is jpg/gif/png
-		function isImgUrl(imageUrl) {
-			const input = new URL(imageUrl);
-			return /\.(jpg|jpeg|png|gif)$/.test(input.pathname);
-		}
-
-		//Event when clicking 'Create Product' button
-		document
-			.getElementBtId("bt-product")
-			.addEventListener("click", function (event) {
-				//Prevent refreshing the screen
-				event.preventDefault();
-
-				//validation if uploaded image is jpg/png/gif
-				if (!isImgUrl(imageURL)) {
-					errorMessage.textContent = "Please enter a valid image URL.";
-					return;
-				}
-
-				//validate that price is not negative.
-				if (price < 0) {
-					errorMessage.textContent = "Please insert price from 0 onward.";
-				}
-
-				//Create new upload object
-				const newUpload = {
-					id: idCounter++,
-					productName: productName,
-					price: price,
-					imageUrl: imageUrl,
-					buys: false,
-				};
-
-				//push new upload into array.
-				upload.push(newUpload);
-
-				//Display the upload object in the UI below
-				displayUpload(newUpload);
-
-				//Clear all error messages(s)
-				errorMessage.textContent = "";
-
-				//Clear all documents in the form
-				document.getElementById("form").reset();
-			});
-
-		//Make function to upload
-		function displayUpload(upload) {
-			const displaySection = document.getElementById("displaySection");
-			const card = document.createElement("div");
-			card.className = "bg-white";
-
-			card.innerHTML = `
-        <img src="${upload.imageURL}" alt="${upload.caption}" class="w-full aspect-[4/3] rounded-md mb-4 object-cover">
-    <p class="text-gray-700 font-semibold text-2xl text-balance overflow-hidden my-4 p-4">${upload.caption}</p>
-    <label class="inline-flex items-center mt-2 text-2xl px-4">
-      <input type="checkbox" class="w-8 h-8 text-2xl accent-rose-600" data-id="${upload.id}" onchange="toggleLike(event)">
-      <span class="ml-2 text-gray-700 font-semibold">Love this 😍!</span>
-    </label>
-        
-        `;
-		}
+		const errorMessage = document.getElementById("errorMessage");
 
 		//validation if uploaded image is jpg/png/gif
-		if (!isImgUrl(imageURL)) {
+		if (!isImgUrl(image)) {
 			errorMessage.textContent = "Please enter a valid image URL.";
 			return;
 		}
@@ -98,6 +53,7 @@ document
 		//validate that price is not negative.
 		if (price < 0) {
 			errorMessage.textContent = "Please insert price from 0 onward.";
+			return;
 		}
 
 		//Create new upload object
@@ -105,7 +61,7 @@ document
 			id: idCounter++,
 			productName: productName,
 			price: price,
-			imageUrl: imageUrl,
+			imageUrl: image,
 			buys: false,
 		};
 
@@ -122,19 +78,23 @@ document
 		document.getElementById("form").reset();
 	});
 
-//Make function to upload
-function displayUpload(upload) {
-	const displaySection = document.getElementById("displaySection");
-	const card = document.createElement("div");
-	card.className = "bg-white";
+//Feature 2: Add ticked items to cart.
 
-	card.innerHTML = `
-        <img src="${upload.imageURL}" alt="${upload.caption}" class="w-full aspect-[4/3] rounded-md mb-4 object-cover">
-    <p class="text-gray-700 font-semibold text-2xl text-balance overflow-hidden my-4 p-4">${upload.caption}</p>
-    <label class="inline-flex items-center mt-2 text-2xl px-4">
-      <input type="checkbox" class="w-8 h-8 text-2xl accent-rose-600" data-id="${upload.id}" onchange="toggleLike(event)">
-      <span class="ml-2 text-gray-700 font-semibold">Love this 😍!</span>
-    </label>
-        
-        `;
+//To toggle Value when click box
+function toggleLike(event) {
+	const checkbox = event.target;
+	const uploadId = parseInt(checkbox.getAttribute("data-id"));
+	const upload = uploads.find((upload) => upload.id === uploadId);
+
+	if (upload) {
+		upload.likes = checkbox.checked;
+		updateLikeCounter();
+	}
+}
+
+function updateLikeCounter() {
+	totalLikes = uploads.filter((upload) => upload.likes).length;
+	document.getElementById(
+		"likeCounter"
+	).textContent = `Total Likes: ${totalLikes}`;
 }
